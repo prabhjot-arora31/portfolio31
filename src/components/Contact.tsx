da29a03caf744ee134cb3efa2c6981e62b891405
Prabhjot Arora <83449967+prabhjot-arora31@users.noreply.github.com>
2024-09-29 16:53:34 +0530

    Update Contact.tsx

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -7,10 +7,10 @@ const Contact = () => {
     <h1 className="font-bold text-2xl mb-7 text-center text-[#0895A1]">Contact</h1>
     <form className=' w-full flex justify-center items-center space-y-9 flex-col'>
        <div className='flex flex-col space-y-2'> <label htmlFor="name">Name</label>
-       <input className='text-black py-2 px-4 rounded' id='name'/></div>
+       <input className='outline-none text-black py-2 px-4 rounded' id='name'/></div>
        <div className='flex flex-col space-y-2'> <label htmlFor="email">Email</label>
-       <input className='text-black py-2 px-4 rounded' id='email'/></div> <div className='flex flex-col space-y-2'> <label htmlFor="msg">Message</label>
-       <textarea id='msg' cols={25} rows={4}> </textarea>
+       <input className='outline-none text-black py-2 px-4 rounded' id='email'/></div> <div className='flex flex-col space-y-2'> <label htmlFor="msg">Message</label>
+       <textarea id='msg' className='outline-none text-black py-2 px-4 rounded' cols={25} rows={4}> </textarea>
     </div>
     <Button variant='custom'>Submit</Button>
     </form>
