c6c99c8fee28293025ac7414d8d92a5963e63ec8
Prabhjot Arora <83449967+prabhjot-arora31@users.noreply.github.com>
2024-09-29 17:08:49 +0530

    Update Hero.tsx

--- src/components/ui/Hero.tsx
+++ src/components/ui/Hero.tsx
@@ -18,8 +18,8 @@ const Hero = () => {
             <p className='mt-10 text-md mb-7'>I am an MCA student with a passion for designing and developing web and mobile applications. </p>
             <div className='mb-5 flex space-x-4'>
                 <Link href='https://linkedin.com/in/prabhjot-arora' className='hover:bg-[#0895A1] flex justify-center items-center hover:text-[#20242D] text-[#0895A1]' style={{borderRadius:"50%",border:'1px solid #0895A1',width:'40px',height:'40px'}}><i className="fa-brands fa-linkedin-in"  ></i></Link>
-                <Button className='hover:bg-[#0895A1] hover:text-[#20242D] text-[#0895A1]' style={{borderRadius:"50%",border:'1px solid #0895A1',width:'40px',height:'40px'}}><i className="fa-brands fa-instagram"  ></i></Button>
-                <Button className='hover:bg-[#0895A1] hover:text-[#20242D] text-[#0895A1]' style={{borderRadius:"50%",border:'1px solid #0895A1',width:'40px',height:'40px'}}><i className="fa-brands fa-github"  ></i></Button>
+                
+                <Link href='https://github.com/prabhjot-arora31' className='hover:bg-[#0895A1] flex justify-center items-center hover:text-[#20242D] text-[#0895A1]' style={{borderRadius:"50%",border:'1px solid #0895A1',width:'40px',height:'40px'}}><i className="fa-brands fa-github"  ></i></Link>
                 </div>
             <a href={'/prabhjot_resume.pdf'} download='prabhjot_resume.pdf' className='flex mt-7 max-w-48 text-center p-2 rounded-3xl' style={{backgroundColor:"#0895A1",color:'black',boxShadow:'3px 3px 20px #0895A1'}}>Download Resume  &nbsp;  <DownloadIcon /></a>
         </div>
